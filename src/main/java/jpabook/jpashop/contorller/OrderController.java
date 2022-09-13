723e78a8a0d998d7063174331098280bbcc22c37
username <wogh0013@naver.com>
2022-09-13 22:04:45 +0900

    commit

--- src/main/java/jpabook/jpashop/contorller/OrderController.java
+++ src/main/java/jpabook/jpashop/contorller/OrderController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,5 +32,13 @@ public class OrderController {
         model.addAttribute("items", items);
 
         return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+        orderService.order(memberId, itemId, count);
+        return "redirect:/";
     }
 }
